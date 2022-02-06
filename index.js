8cac819d27ecb4b078f4f0d733669efc3be3ce5d
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-02-07 03:41:00 +1100

    after successfully testing if node was working, added questions to list

--- index.js
+++ index.js
@@ -8,7 +8,46 @@ const questions = [
     {
         type: 'input',
         message: 'What is the title of your project?',
-        name: title
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Give a description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What is the installation information regarding your project?',
+        name: 'iInfo',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage information regarding your project?',
+        name: 'uInfo',
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidlines of your project?',
+        name: 'cInfo',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions of your project?',
+        name: 'tInfo',
+    },
+    {
+        type: 'list',
+        message: 'What kind of license is your project under?',
+        choices: [
+            'Apache License 2.0',
+            'GNU General Public License v3.0',
+            'MIT',
+            'Creative Commons Zero v1.0 Universal',
+            'Eclipse Public License',
+            'Mozilla Public Licence 2.0',
+            'The Unlicense'
+        ],
+        name: 'lInfo',
     }
 
 ];
